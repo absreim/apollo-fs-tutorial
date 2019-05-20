bcd8d64f007b44b7b7aefb70f6291d860d9f9799
Brook Li <absreim@github.com>
2019-05-20 00:12:50 -0400

    Section 3 finished.

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -1,16 +1,30 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
 
 class LaunchAPI extends RESTDataSource {
+
   constructor() {
     super()
     this.baseURL = 'https://api.spacexdata.com/v2/'
   }
+
   async getAllLaunches(){
     const response = await this.get('launches')
     return Array.isArray(response) ?
       response.map(launch => this.launchReducer(launch)) :
       []
   }
+
+  async getLaunchById({ launchId }) {
+    const response = await this.get('launches', { flight_number: launchId })
+    return this.launchReducer(response[0])
+  }
+
+  getLaunchesByIds({ launchIds }) {
+    return Promise.all(
+      launchIds.map(launchId => this.getLaunchById({ launchId }))
+    )
+  }
+
   launchReducer(launch) {
     return {
       id: launch.flight_number || 0,
